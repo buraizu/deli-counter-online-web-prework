54f49b8719c339214df283b2df9c465ba8b5aadf
Bryce Eadie <bryce.eadie@gmail.com>
2018-09-28 07:37:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def take_a_number(line_array, name)
   line_array.push(name)
   line_array.each_with_index{|name, index|
-    puts "#{name} "
+    puts "Welcome, #{name}. You are number #{index + 1} in line."
   }
 end 
 
