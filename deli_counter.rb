78278a1cd2bef6bad63e40e9155b4627882b32a5
Bryce Eadie <bryce.eadie@gmail.com>
2018-09-28 07:30:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,14 @@
 katz_deli = []
 
+def take_a_number(line_array, name)
+  line_array.push(name)
+  line_array.each do |element|
+    puts "#{element} "
+end 
+
 def line (array)
   if array.length == 0 
     puts "The line is currently empty."
   end
+  
 end 
