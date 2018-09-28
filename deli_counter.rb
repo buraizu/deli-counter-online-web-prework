c8bf7fab3615dbba2fa36d75d66ed8c5fa367bd8
Bryce Eadie <bryce.eadie@gmail.com>
2018-09-28 08:01:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,5 +13,10 @@ def line (array)
   if array.length == 0 
     puts "The line is currently empty."
   end
-  
 end 
+
+def now_serving(array)
+  if array.length == 0 
+    puts "There is nobody waiting to be served!"
+  end 
+end
